5ae6fcaee168a15bec1e4ffa29eb82227a88e0ae
Rahul Malhotra <rahulmalhotra9620@yahoo.in>
2020-07-26 12:08:59 +0530

    Added Todo Item to the Todo List

--- force-app/main/default/lwc/todo/todo.js
+++ force-app/main/default/lwc/todo/todo.js
@@ -1,5 +1,24 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, track } from 'lwc';
 
 export default class Todo extends LightningElement {
+
+    @track
+    todoTasks = [];
+
+    newTask = '';
+
+    updateNewTask(event) {
+        this.newTask = event.target.value;
+    }
+
+    addTaskToList(event) {
+        // * Unshift function - used to add element at the beginning of the array
+        // * Push function - used to add element at the end of the array
+        this.todoTasks.push({
+            id: this.todoTasks.length + 1,
+            name: this.newTask
+        });
+        this.newTask = '';
+    }
 
 }
