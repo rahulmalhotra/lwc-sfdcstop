ac58dd2985fe6b256d9ddf3aa458a1d68db8e529
Rahul Malhotra <rahulmalhotra9620@yahoo.in>
2020-08-22 11:07:25 +0530

    Final Commit

--- force-app/main/default/lwc/todo/todo.js
+++ force-app/main/default/lwc/todo/todo.js
@@ -7,6 +7,8 @@
 import { LightningElement, track, wire } from 'lwc';
 import getTasks from '@salesforce/apex/ToDoListController.getTasks';
 import { refreshApex } from '@salesforce/apex';
+import insertTask from '@salesforce/apex/ToDoListController.insertTask';
+import deleteTask from '@salesforce/apex/ToDoListController.deleteTask';
 
 export default class Todo extends LightningElement {
 
@@ -15,6 +17,9 @@ export default class Todo extends LightningElement {
     todoTasks = [];
 
     todoTasksResponse;
+
+    // * Setting processing variable to true initially
+    processing = true;
 
     // * Variable to store the new task that you want to add to the list
     newTask = '';
@@ -34,6 +39,11 @@ export default class Todo extends LightningElement {
     */
     addTaskToList(event) {
 
+        // * If the task name is empty, return
+        if(this.newTask=='') {
+            return;
+        }
+
         /*
         *   Unshift function - used to add element at the beginning of the array
         *   Uncomment this to use the unshift function and comment the below push function
@@ -45,12 +55,25 @@ export default class Todo extends LightningElement {
         });
         */
 
-        // * Push function - used to add element at the end of the array
-        this.todoTasks.push({
-            id: this.todoTasks.length + 1,
-            name: this.newTask
-        });
-        this.newTask = '';
+        // * Set processing to true
+        this.processing =  true;
+
+        // * Insert the task in Salesforce
+        insertTask({ subject: this.newTask })
+        .then(result => {
+            console.log(result);
+            // * Push function - used to add element at the end of the array
+            this.todoTasks.push({
+                id: this.todoTasks[this.todoTasks.length - 1] ? this.todoTasks[this.todoTasks.length - 1].id + 1 : 0,
+                name: this.newTask,
+                recordId: result.Id
+            });
+            this.newTask = '';
+            console.log(JSON.stringify(this.todoTasks));
+        })
+        .catch(error => console.log(error))
+        .finally(() => this.processing = false);
+        // * Set processing variable to false after receiving response from salesforce
     }
 
     /*
@@ -62,6 +85,10 @@ export default class Todo extends LightningElement {
         let idToDelete = event.target.name;
         let todoTasks = this.todoTasks;
         let todoTaskIndex;
+        let recordIdToDelete;
+
+        // * Set processing variable to true
+        this.processing = true;
 
         /*
         *   Method 1 - Finding the index of the task to be deleted
@@ -73,8 +100,23 @@ export default class Todo extends LightningElement {
             }
         }
 
-        // * Comment the below line if you're using one of the two approaches given below
-        todoTasks.splice(todoTaskIndex, 1);
+        recordIdToDelete = todoTasks[todoTaskIndex].recordId;
+
+        // * Deleting task in Salesforce
+        deleteTask({ recordId: recordIdToDelete })
+        .then(result => {
+            console.log(result);
+            if(result) {
+                // * Comment the below line if you're using one of the two approaches given below
+                todoTasks.splice(todoTaskIndex, 1);
+            } else {
+                console.log('Unable to delete task');
+            }
+            console.log(JSON.stringify(this.todoTasks));
+        })
+        .catch(error => console.log(error))
+        .finally(() => this.processing = false);
+        // * Set processing variable to false after receiving response from salesforce
 
         /*
         *   Un-Comment any one of the two below methods
@@ -108,6 +150,12 @@ export default class Todo extends LightningElement {
         this.todoTasksResponse = response;
         let data = response.data;
         let error = response.error;
+
+        // * Set processing variable to false if response is received from salesforce
+        if(data || error) {
+            this.processing = false;
+        }
+
         if(data) {
             console.log('data');
             console.log(data);
@@ -130,11 +178,15 @@ export default class Todo extends LightningElement {
     *   i.e. todoTasks in the browser cache
     */
     refreshTodoList() {
+        // * Set processing variable to true
+        this.processing = true;
         /*
         *   It'll refresh the data in browser cache only
         *   if there is a change on the server side
         */
-        refreshApex(this.todoTasksResponse);
+        refreshApex(this.todoTasksResponse)
+        .finally(() => this.processing = false);
+        // * Set processing variable to false after receiving response from salesforce
     }
 
 }
